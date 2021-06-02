6abccc8010c88eaa3d41825266f2175226892b59
Ramkumar S P <rampandi@publicisgroupe.net>
2021-06-02 11:24:09 +0530

    adding the find and flatmap code

--- src/main/java/com/practise/java/pojo/Individual.java
+++ src/main/java/com/practise/java/pojo/Individual.java
@@ -1,13 +1,22 @@
 package com.practise.java.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Individual {
     private String name;
     private List<Vehicle> vehicles;
 
-    public Individual(String name, List<Vehicle> vehicles) {
-        this.name = name;
-        this.vehicles = vehicles;
-    }
+//    public Individual(String name, List<Vehicle> vehicles) {
+//        this.name = name;
+//        this.vehicles = vehicles;
+//    }
+
 }
