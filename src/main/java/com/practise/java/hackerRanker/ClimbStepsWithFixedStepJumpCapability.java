a8ebf8c85deb67d42c926074bb751dd56e879c1e
ramkumarsp <ramkumarsp.practice@gmail.com>
2021-11-22 00:04:06 +0530

    adding new files

---
+++ src/main/java/com/practise/java/hackerRanker/ClimbStepsWithFixedStepJumpCapability.java
@@ -0,0 +1,12 @@
+package com.practise.java.hackerRanker;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClimbStepsWithFixedStepJumpCapability {
+    public static void main(String[] args) {
+//        Set<Integer> jumpPosibilities =
+                Arrays.asL
+    }
+}
